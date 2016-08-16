cc6234b4aeadefd956902afa768ff2c70bea151d
yunxingwoo <wuyunxingl@gmail.com>
2016-08-16 19:46:05 +0800

    new push

--- Mysite/urls.py
+++ Mysite/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 import TDD.views
+
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     url(r'^$',TDD.views.home_page),
